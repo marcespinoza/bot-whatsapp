49ad5036df2b03c60087d6b7718470c7ae42c837
marcespinoza <you@example.com>
2022-12-10 08:26:09 -0300

    Se termina registro de jugador

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const xl = require('excel4node');
 const wb = new xl.Workbook();
 const ws = wb.addWorksheet('Worksheet Name'); 
 const { google } = require("googleapis");
+const { datacatalog } = require('googleapis/build/src/apis/datacatalog');
 
 
 venom
@@ -16,79 +17,115 @@ venom
   });
 
 function start(client) {
+
+  var dataPlayer = new Array();
+
   client.onMessage(async (message) => {
+
+    const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
   
+    // Create client instance for auth
+    const gclient = await auth.getClient();
   
+    // Instance of Google Sheets API
+    const googleSheets = google.sheets({ version: "v4", auth: gclient });
+  
+    const spreadsheetId = "1927xBquC0QfIBcOHXZ93LfXGbz1RRENP9e3nWXQP3n4";
+
+    getPlayersFromExcel(auth, googleSheets, spreadsheetId).then(val => {
+
+      if (val.length) {
+        let player_ = findPlayer(val, message.from); 
+              if(player_ !== null){
+                    //Jugador registrado
+                    client.sendText(message.from, 'Hola '+player_[1]+', bienvenido nuevamente 🤝');
+              }else{
+                  registrarJugador(client,message, dataPlayer, auth, googleSheets, spreadsheetId);
+              }
+        } else {
+              registrarJugador(client,message, dataPlayer, auth, googleSheets, spreadsheetId);
+        }
+    });
+
+ 
   });
 }
 
-  async function showWelcomeButtons(){
+function registrarJugador(client, message, dataPlayer,auth, googleSheets, spreadsheetId){
+  if (message.body === 'Hola' && message.isGroupMsg === false) {
+    dataPlayer.push(message.from);
+    client.sendText(message.from, 'Bienvenido al bot del Torneo. Voy a registrar tus datos')
+   .then((result) => {
+        client.sendText(message.from, '👉 *Citá este mensaje con tu nombre*')
+        .then((result) => {
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    })
+  .catch((erro) => {
+    console.error('Error when sending: ', erro); //return object error
+  });
+    }else{
+      if(message.type === 'reply' && message.quotedMsg.body==='👉 *Citá este mensaje con tu nombre*'){
+          client.sendText(message.from, 'Genial '+message.body+' ya registramos tu nombre').then((result) => {
+              dataPlayer.push(message.body);
+              client.sendText(message.from, '👉 *Ahora citá este mensaje con tu apellido*').
+              then((result) =>{}).catch((error) => console.log(error))
+          }).catch((erro) => {
+            console.error('Error when sending: ', erro); //return object error
+          });
 
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-      });
-    
-      // Create client instance for auth
-      const client = await auth.getClient();
-    
-      // Instance of Google Sheets API
-      const googleSheets = google.sheets({ version: "v4", auth: client });
-    
-      const spreadsheetId = "1927xBquC0QfIBcOHXZ93LfXGbz1RRENP9e3nWXQP3n4";
-    
-      // Get metadata about spreadsheet
-      const metaData = await googleSheets.spreadsheets.get({
-        auth,
-        spreadsheetId,
-      });
-    
-      // Read rows from spreadsheet
-      const getRows = await googleSheets.spreadsheets.values.get({
-        auth,
-        spreadsheetId,
-        range: "inscriptos!A:A",
-      });
-    
-      //Write row(s) to spreadsheet
-      await googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId,
-        range: "inscriptos!A:F",
-        valueInputOption: "USER_ENTERED",
-        resource: {
-          values: {values:['marcelo']},
-        },
-      });
-    
-      console.log("Successfully submitted! Thank you!"+getRows.data);
+      }
+      if(message.type === 'reply' && message.quotedMsg.body==='👉 *Ahora citá este mensaje con tu apellido*'){
+        client.sendText(message.from, 'Ya registramos tus datos').then((result) => {
+            dataPlayer.push(message.body);
+            saveDataInExcel(auth, spreadsheetId, googleSheets, dataPlayer)
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+
+    }  
+    }
+  }  
+ 
+  function findPlayer(players, user){
+    let player = null;
+    if (players.length) {
+            players.map((row) => {
+              if(row[0] === user){
+                player = row;
+              }
+            });
+            return player;
+    }
   }
 
-  const headingColumnNames = [
-    "Name",
-    "Email",
-    "Mobile",
-]
-const data = [
-    {
-      "name":"Shadab Shaikh",
-      "email":"shadab@gmail.com",
-      "mobile":"1234567890"
-    }
-  ]
-
-let headingColumnIndex = 1;
-headingColumnNames.forEach(heading => {
-    ws.cell(1, headingColumnIndex++)
-        .string(heading)
-});
-let rowIndex = 2;
-data.forEach( record => {
-    let columnIndex = 1;
-    Object.keys(record ).forEach(columnName =>{
-        ws.cell(rowIndex,columnIndex++)
-            .string(record [columnName])
+  async function getPlayersFromExcel(auth, googleSheets,spreadsheetId){
+    // Read rows from spreadsheet
+    const res =await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "inscriptos!A:C",
     });
-    rowIndex++;
-});
-wb.write('filename.xlsx');
+    return res.data.values;
+  }
+
+  async function saveDataInExcel(auth, spreadsheetId, googleSheets, dataPlayer){
+    //Write row(s) to spreadsheet
+   let res = await googleSheets.spreadsheets.values.append({
+      auth,
+      spreadsheetId,
+      range: "inscriptos!A:C",
+      valueInputOption:'RAW',
+      valueInputOption: "USER_ENTERED",
+      resource:{
+        values:[dataPlayer]
+      }
+    });
+    res
+  }
